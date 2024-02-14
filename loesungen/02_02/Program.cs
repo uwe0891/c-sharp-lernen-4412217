5a259c2f4c09738dc050172bc0e822b26f538b8c
uwe0891 <160032541+uwe0891@users.noreply.github.com>
2024-02-14 14:33:04 +0000

    Pending changes exported from your codespace

--- loesungen/02_02/Program.cs
+++ loesungen/02_02/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Anlegen und initialisieren der ersten Variable
-int meine_erste_variable = 11;
+int meine_erste_variable = 5;
 
 // Anlegen und initialisieren der zweiten Variable
-int meine_zweite_variable = 10;
+int meine_zweite_variable = 6;
 
 // Berechnung der Summe
 int mein_Ergebnis = meine_erste_variable + meine_zweite_variable;
